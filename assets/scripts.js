a005a1b90743e3ec9b56d009ff2add50c1eb7328
Marcus <mm95861p@pace.edu>
2022-11-16 14:41:50 -0500

    lab8

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,23 +1,40 @@
 let currentPrice=0, itemCount=0
 
 function addToCart(cookie) {
-    /* 
-        PRICES
-        peanut butter: 20
-        sandies: 30
-        party press: 35
-        chocolate chip: 25
+    /*PRICES
+    peanut-butter:20
+    sandies:30
+    party press:35
+    chcocolate chip:25
     */
+
+    console.log('The user is ading this type of cookie to their cart: ' , cookie)
+
+    //add 1 to the itemcount vairable
+
+    itemCount = itemCount+1
+    console.log(itemCount)
+
+
+
+    //add the correct price to the currentPrice variable
+
+    if (cookie == "peanut butter"){
+        currentPrice == currentPrice + 20
+    } else if (cookie == "sandles"){
+        currentPrice ==currentPrice +30
+    } else if (cookie == "party press"){
+    currentPrice = currentPrice + 35 
+    } else if (cookie == "chocloate chip"){
+    currentPrice = currentPrice = 25
+    }
    
-   console.log('The user is adding this type of cookie to their cart: ' , cookie) 
-
-   //add 1 to the itemCount variable
-   //add the correct price to the currentPrice variable
-
+    console.log(currentPrice)
 }
+
 
 function checkout() {
     console.log('User is checking out.')
-    //Let your customer know how many items they are purchasing and the price
-    
+    //let your customer know how many items they are purchasing and the price 
+window.alert(`You have a total of ${itemCount} items that will cost ${currentPrice}. Thank you for shopping with us `)
 }
