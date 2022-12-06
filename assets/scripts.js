ad7a4193d1ced2de4a9214c2d5f7f635c6f54cd9
Marcus <mm95861p@pace.edu>
2022-12-05 19:40:08 -0500

    lab9

--- assets/scripts.js
+++ assets/scripts.js
@@ -15,19 +15,21 @@ function addToCart(cookie) {
     itemCount = itemCount+1
     console.log(itemCount)
 
-
+    document.getElementById("cartItems").innerHTML=itemCount
 
     //add the correct price to the currentPrice variable
 
-    if (cookie == "peanut butter"){
+    if (cookie == 'peanut butter'){
         currentPrice == currentPrice + 20
-    } else if (cookie == "sandles"){
+    } else if (cookie == 'sandles'){
         currentPrice ==currentPrice +30
-    } else if (cookie == "party press"){
+    } else if (cookie == 'party press'){
     currentPrice = currentPrice + 35 
-    } else if (cookie == "chocloate chip"){
+    } else if (cookie == 'chocloate chip'){
     currentPrice = currentPrice = 25
     }
+   
+   document.querySelector(".hoverText").innerHTML = currentPrice
    
     console.log(currentPrice)
 }
@@ -37,4 +39,21 @@ function checkout() {
     console.log('User is checking out.')
     //let your customer know how many items they are purchasing and the price 
 window.alert(`You have a total of ${itemCount} items that will cost ${currentPrice}. Thank you for shopping with us `)
+
+currentPrice=0
+itemCount=0
+
+document.getElementById("cartItems").innerHTML=itemCount
+document.querySelector(".hoverText").innerHTML=currentPrice
+
 }
+
+function darkMode() {
+    // add your code here
+
+    document.querySelector("body").stylebackgroundColor="black"
+    document.querySelector("main").style.color="white"
+    document.querySelector("h1").style.color="white"
+}
+
+
