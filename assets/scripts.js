670056de9f6c6f7de8ad5061f71d5e3dca4f48b8
Marcus <mm95861p@pace.edu>
2022-12-14 14:35:35 -0500

    lab10

--- assets/scripts.js
+++ assets/scripts.js
@@ -14,9 +14,8 @@ clear:function() {
     //reset the currentPrice and items properties
     this.currentPrice=0
     this.items=[]
+},
 }
-}
-let currentPrice=0, itemCount=0
 
 function addToCart(cookie) {
     /*PRICES
@@ -30,43 +29,33 @@ function addToCart(cookie) {
 
     //add 1 to the itemcount vairable
 
-    itemCount = itemCount+1
-    console.log(itemCount)
+    
 
-    document.getElementById("cartItems").innerHTML=itemCount
+    
 
     //add the correct price to the currentPrice variable
 
-    if (cookie == 'peanut butter'){
-    currentPrice == currentPrice + 20
-    } else if (cookie == 'sandles'){
-    currentPrice ==currentPrice +30
-    } else if (cookie == 'party press'){
-    currentPrice = currentPrice + 35 
-    } else if (cookie == 'chocloate chip'){
-    currentPrice = currentPrice = 25
-    }
+    if (cookie == 'peanut butter'){cart.additem(cookie,20)}
+    if (cookie == 'sandles'){cart.additem(cookie, 30)}
+    if (cookie == 'party press') {cart.additem(cookie,35)}
+    if (cookie == 'chocolate chip') { cart.adddItem(cookie,25}
    
-   document.querySelector(".hoverText").innerHTML = currentPrice
-   
-    console.log(cart)
-
-    document.getElementById("cartItems").InnerHTML=cart.items.length
+   document.querySelector(".hoverText").innerHTML = cart.currentPrice
+   console.log(cart)
+   document.getElementById("cartItems").InnerHTML=cart.items.length
 }
+functioncheckout()
+    {
 
-
-function checkout() {
     console.log('User is checking out.')
     //let your customer know how many items they are purchasing and the price 
 window.alert(`You have a total of ${itemCount} items that will cost ${currentPrice}. Thank you for shopping with us `)
-
-currentPrice=0
-itemCount=0
-
-document.getElementById("cartItems").innerHTML=itemCount
-document.querySelector(".hoverText").innerHTML=currentPrice
-
-}
+prompt("Please provide your name and address. /n Thank you for shopping with us!")
+cart.clear()
+document.querySelector(".hoverText").innerHTML = cart.currentPrice
+console.log(cart)
+document.getElementById("cartItems").innerHTML = cart.items.length
+    }
 
 function darkMode() {
     // add your code here
